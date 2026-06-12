fd60ad936069515976d0433942b007a29a3bfe5a
shihuili1218 <gege87417376@qq.com>
2026-06-12 22:33:51 +0800

    ip -> hostname

--- jepsen.atomic/run_test.sh
+++ jepsen.atomic/run_test.sh
@@ -1,2 +1,2 @@
 unset SSH_AUTH_SOCK
-lein run test --username root --password xxx --nodes-file  ./nodes  --ssh-private-key /xxx/.ssh/id_rsa $@
+lein run test --username root --password xxx --nodes-file ./nodes --ssh-private-key /xxx/.ssh/id_rsa $@
